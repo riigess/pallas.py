58eb2fc0e59b4cc5f7665cf1da268e0d3027f331
Austin Bennett <56428356+Riigess@users.noreply.github.com>
2024-09-01 15:10:48 -0700

    Removed scripts/

--- scripts/core/AudienceLookups.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class AudienceLookups(Enum):
-    macos_generic = "02d8e57e-dd1c-4090-aa50-b4ed2aef0062"
-    macos_ifplanner = "49f27e9a-6789-4d88-b13e-93032da82c6b" #Reserach - com.apple.if.planner.nlrouter.[tokenizer|base].en
-    ios_generic = "0c88076f-c292-4dad-95e7-304db9d29d34"
-    tvos_generic = "fe6f26f9-ec98-46d2-8faf-565375a83ba7"
-    visionpro_generic = "5cb41593-0f8a-45ba-89c6-52928b9caaae"
-    watchos_generic = "fe4c7f1c-f44c-4c00-b3df-eef225a1ac9d"
